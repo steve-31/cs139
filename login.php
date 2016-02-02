d299e858496e30468c28ee95b4ec5ccb9e32a69a
steve-31 <stephen.lynn318@gmail.com>
2016-02-02 17:15:00 +0000

    include js file

--- login.php
+++ login.php
@@ -6,6 +6,7 @@
 		<link rel="stylesheet" type="text/css" href="home.css">
 		<link href='https://fonts.googleapis.com/css?family=Roboto:400,900' rel='stylesheet' type='text/css'>
 		<link href='https://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
+		<script type="text/javascript" src="script.js"></script>
 	</head>
 	<body>
 		<div id="header" style="height:120px;">
