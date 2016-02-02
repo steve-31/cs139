ac4822a1283af4ad84d77363d9ec1fba6350b3bd
steve-31 <stephen.lynn318@gmail.com>
2016-02-02 17:02:02 +0000

    Create LandingPage.php

---
+++ LandingPage.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>My lists</title>
+	<meta charset="UTF-8">
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:300|Roboto:400,900' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="home.css">
+	<!-- Roboto bold and Open sans fonts are now available -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+	<script type="text/javascript" src="script.js"></script>
+</head>
+
+<body>
+
+  <div id="header" style="height:50px;">
+      <img id="logo" src="MagikListBolt.jpg" height="50px" alt="MagikList Logo">
+       <div class="logout"> <a href="#">Logout</a> </div>
+       <div id="usermenu">
+      <ul>
+		<li> <a href="LandingPage.html">Home</a> </li>
+		<li> <a href="#">Notifications</a> </li>
+		<li> <a href="#">My Lists</a> </li>
+      </ul>
+   </div>
+  </div>
+
+  
+  <div class="usercontent fadein">
+		<h1 class="title"> Welcome "current username"</h1>
+		<div class="left">
+			<h2>All Lists</h2>
+				<ul class="lists">
+					<li>Home<li>
+						<ul>
+							<li>shopping List</li>
+							<li>Housework</li>
+						</ul>
+					<li>Work</li>				
+				
+				</ul> 
+	
+		</div>
+		
+		<div class="right">
+			<h2>My Recent Lists</h2>
+
+						<a href="#"><div class="recentused">
+		
+							</div></a>
+						<a href="#"><div class="recentused">
+		
+							</div></a>
+						<a href="#"><div class="recentused">
+		
+							</div></a>
+	
+		</div>
+  </div>
+  <footer class="bottomBar">
+  		<div id="nav">
+      <ul>
+		<li> <a href="#">Contact Us</a> </li>
+		 | <li>©Steve&Sylvester 2016</li>
+      </ul>
+   </div>
+  </footer>
+  
+	</body>
+
+</html>
