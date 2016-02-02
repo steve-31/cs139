f88df5e3176789ac57d54d474019b3f0413738c4
steve-31 <stephen.lynn318@gmail.com>
2016-02-02 17:09:02 +0000

    change nav bar to 'register' instead of 'sign up'

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
    	<div id="nav">
       <ul>
 		<li> <a href="index.php">Home</a> </li>
-		<li> <a href="registration.php">Sign up</a> </li>
+		<li> <a href="registration.php">Register</a> </li>
 		<li> <a href="login.php">Login</a> </li>
       </ul>
     </div>  ' ;
