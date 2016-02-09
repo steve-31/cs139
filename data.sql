265e2d0b33a5292f74b0021825575384fddf972a
Silverstre <sbcardorelle@hotmail.co.uk>
2016-02-09 17:18:00 +0000

    Create data.sql
    contains dummy data for database

---
+++ data.sql
@@ -0,0 +1,10 @@
+INSERT INTO users(Title,Firstname,Surname,Username,Email,Password)  
+VALUES ('Master','Sylvester','Cardorelle','Silverrxx12312','mrstealyogirl@hotmail.co.uk','4213412iwjej9032rwor3902uroasfjafo');
+
+INSERT INTO users(Title,Firstname,Surname,Username,Email,Password)  
+VALUES ('Mrs','Avnit','Patel','Stormzy','adidas@tracksuit.com','923uirwhdsakhd98ew3');
+
+INSERT INTO Lists(userID,Name,LastEdit,Category) VALUES (1,'homework','2012-03-24 17:45:12','work');
+
+INSERT INTO Items(ListID,Content,Done,DueDate) VALUES (1,'do my cs139',0,'2016-12-06 17:45:12') ;
+
