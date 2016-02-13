bfb84cd65899074e55d43f533badec63c4286001
steve-31 <stephen.lynn318@gmail.com>
2016-02-13 14:25:08 +0000

    login works

--- login.php
+++ login.php
@@ -10,8 +10,10 @@
 		<script type="text/javascript" src="script.js"></script>
 	</head>
 		<?php
-		include 'encrypt.php'
-		include 'db_connect.php'
+		session_start();
+		include 'encrypt.php';
+		include 'database.php';
+		$error = "";
 		//if a user submits the form 
 			if ($_SERVER["REQUEST_METHOD"] == "POST") {
 					//validate information
@@ -22,44 +24,26 @@
 					//query to fetch password from db
 					$query = "SELECT Password FROM users WHERE Username = '$username'" ;
 					//connect to database
-                          try {
-                               $conn = new PDO("mysql:host=$servername;dbname=$database", $database_username, $database_password);
-                               // set the PDO error mode to exception
-                               $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                          }
-                          catch(PDOException $e){
-                               echo "Connection failed: " . $e->getMessage();
-                          }
+                    $db = new SQLite3('todo.db');
                     //run query
-                    $db_password = $conn->query($query) ;
+                    $db_password = $db->querySingle($query) ;
                     //check passwords match
-                    		if ($hash==$db_password) {
-                    			session_start() ;
-                    			$session_['username'] = $username ;
-                    			header(Location: 'LandingPage.php') ;
+                    		if ($hash == $db_password) {
+                    			$_SESSION['username'] = $username ;
+                    			echo "<script> window.location.assign('LandingPage.php'); </script>";
                     		}
                     	    else{
-                    	    	$error="Username or password is incorrect, please try again" ;
+
+                    	    	$error="Username or Password Incorrect. Please Try Again." ;
                     	    }
-
-
-
-
+                    	    
+                } 
             function test_input($data) {
               $data = trim($data);
               $data = stripslashes($data);
               $data = htmlspecialchars($data);
               return $data;
             }
-
-
-
-
-
-			}
-
-
-
 	?>
 	<body>
 		<div id="header" style="height:120px;">
@@ -68,7 +52,7 @@
   		<div class="content fadein">
   			<div id="login">
   			<br>
-				<form action="#" method="post">
+				<form action="login.php" method="post">
 					<input type="text" name="username" placeholder="Username"><span style="color:red;"> <?php echo $error ; ?></span></span><br>
 					<input type="password" name="password" placeholder="Password"><br>
   		
