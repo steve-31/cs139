f31d3cff9bea52960f64778edad834d165a3e884
steve-31 <stephen.lynn318@gmail.com>
2016-02-15 17:14:03 +0000

    Create userHeader.php

---
+++ userHeader.php
@@ -0,0 +1,11 @@
+<div id="header" style="height:50px;">
+      <img id="logo" src="MagikListBolt.jpg" height="50px" alt="MagikList Logo">
+       <div class="logout"> <a href="logout.php">Logout</a> </div>
+       <div id="usermenu">
+      <ul>
+		<li> <a href="LandingPage.php">Home</a> </li>
+		<li> <a href="#">Notifications</a> </li>
+		<li> <a href="#">My Lists</a> </li>
+      </ul>
+   </div>
+  </div>
