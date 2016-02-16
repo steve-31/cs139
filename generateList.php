ee07d2638896c21c9d842f98076234357aa1f325
Silverstre <sbcardorelle@hotmail.co.uk>
2016-02-16 18:23:35 +0000

    Update generateList.php
    working generate list, that can now update delete and add list items. (Y)

--- generateList.php
+++ generateList.php
@@ -9,8 +9,41 @@
 <script type="text/javascript" src="script.js"></script>
 </head>
 <?php
+	
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		// if update was clicked
+		if (isset($_POST['update'])) {
+				$content = $_POST['content'] ;
+				$itemID = $_POST['ItemID'] ;
+				$date = $_POST['dueDate'] ;
+				$db = new SQLite3('todo.db') ;
+				$update = "UPDATE items SET Content = '$content',DueDate = '$date' WHERE ItemID = '$itemID' " ;
+				$db->exec($update) ;
+				echo "<script> window.location.assign('LandingPage.php'); </script>";
+		}
+		// if delete was clicked
+		if (isset($_POST['done'])) {
+				$content = $_POST['content'] ;
+				$itemID = $_POST['ItemID'] ;
+				$db = new SQLite3('todo.db') ;
+				$delete = "DELETE FROM Items WHERE ItemID = '$itemID' " ;
+				$db->exec($delete) ;
+				echo "<script> window.location.assign('LandingPage.php'); </script>";
+		}
+		// if add was clicked 
+		if (isset($_POST['add'])) {
+				$content = $_POST['newItem'] ;
+				$listID = $_POST['listID1'] ;
+				$date = $_POST['date'] ;
+				$db = new SQLite3('todo.db') ;
+				$new = "INSERT INTO Items  VALUES(null,'$listID','$content',0,'$date') " ;
+				$db->exec($new) ;
+				echo "<script> window.location.assign('LandingPage.php'); </script>";
+		}
 
 
+		
+	}
 ?>
 <body>
 <div id="header"> 
@@ -36,11 +69,21 @@
 							echo '<tr>' ;
 					echo '<td> <input type="hidden" name="ItemID" value = '.$row["ItemID"].'> </td>' ;
 					echo "<td> <input type='text' name='content' value = '".$row['Content']."''> </td>" ;
+					echo "<td> <input type='text' name='dueDate' value = '".$row['DueDate']."''> </td>" ;
 					echo '<td> <button type="submit" name="update" value="update">update</button> </td>' ;
-					echo '<td> <button type="submit" name="delete" value="delete">delete</button> </td>' ;
+					echo '<td> <button type="submit" name="done" value="done">done</button> </td>' ;
 							echo '</tr>' ;
 							echo '</form>';
 						}
+							echo '<form action="generateList.php" method="post">' ;
+							echo '<tr>' ;
+					echo '<td> <input type="hidden" name="listID1" value = "'.$listID.'""> </td>' ;
+					echo '<td> <input type="text" name="newItem" placeholder = "Add a new item" > </td>' ;
+					echo '<td> <input type="date" name="date"> </td>' ;
+					echo '<td> <button type="submit" name="add" value="add">add</button> </td>' ;
+							echo '</tr>' ;
+							echo '</form>';
+
 						echo '</table>' ;
 
 								
