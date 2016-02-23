6e8859911db8fa5e8531b2419e381fe1845c67dc
steve-31 <stephen.lynn318@gmail.com>
2016-02-23 17:48:12 +0000

    Update generateList.php

--- generateList.php
+++ generateList.php
@@ -15,9 +15,13 @@
 		if (isset($_POST['update'])) {
 				$content = $_POST['content'] ;
 				$itemID = $_POST['ItemID'] ;
+				$listID = $_POST['listID1'] ;
 				$date = $_POST['dueDate'] ;
 				$db = new SQLite3('todo.db') ;
-				$update = "UPDATE items SET Content = '$content',DueDate = '$date' WHERE ItemID = '$itemID' " ;
+				$update = "UPDATE Items SET Content = '$content',DueDate = '$date' WHERE ItemID = '$itemID' " ;
+				$last_edit3 = date('Y-m-d H:i:s') ;
+				$updatelist3 = "UPDATE Lists SET LastEdit = '$last_edit3' WHERE ListID = '$listID' ";
+				$db->exec($updatelist3) ;
 				$db->exec($update) ;
 				echo "<script> window.location.assign('LandingPage.php'); </script>";
 		}
@@ -25,8 +29,12 @@
 		if (isset($_POST['done'])) {
 				$content = $_POST['content'] ;
 				$itemID = $_POST['ItemID'] ;
+				$listID = $_POST['listID1'] ;
 				$db = new SQLite3('todo.db') ;
 				$delete = "DELETE FROM Items WHERE ItemID = '$itemID' " ;
+				$last_edit2 = date('Y-m-d H:i:s') ;
+				$updatelist2 = "UPDATE Lists SET LastEdit = '$last_edit2' WHERE ListID = '$listID' ";
+				$db->exec($updatelist2) ;
 				$db->exec($delete) ;
 				echo "<script> window.location.assign('LandingPage.php'); </script>";
 		}
@@ -37,37 +45,35 @@
 				$date = $_POST['date'] ;
 				$db = new SQLite3('todo.db') ;
 				$new = "INSERT INTO Items  VALUES(null,'$listID','$content',0,'$date') " ;
+				$last_edit = date('Y-m-d H:i:s') ;
+				$updatelist = "UPDATE Lists SET LastEdit = '$last_edit' WHERE ListID = '$listID' ";
+				$db->exec($updatelist) ;
 				$db->exec($new) ;
 				echo "<script> window.location.assign('LandingPage.php'); </script>";
 		}
-
-
 		
 	}
 ?>
 <body>
-<div id="header"> 
-	<h3>List: <?php echo $_POST['name'] ; ?> </h3> 
-</div>
-<div class="content fadein">
+<?php include 'userHeader.php' ; ?>
+	
+<div class="usercontent fadein">
+	<h1 class="title">List: <?php echo $_POST['name'] ; ?> </h1> 
 		<div id="form">
     			<?php 
    					 // using the List_ID and Listname we will output all of the list item information 
 						$username = $_SESSION['username'] ;
-						$listID = $_POST['listID'] ;
+						$listID = $_POST['ListID'] ;
 						$listName = $_POST['name'] ;
-
 						$query = "SELECT * FROM Items WHERE ListID = '$listID'" ;
-
 						$db = new SQLite3('todo.db') ;
-
 						$results = $db->query($query) ;
-
 						echo '<table>' ;
 						while ($row = $results->fetchArray()) {
 							echo '<form action="generateList.php" method="post">' ;
 							echo '<tr>' ;
 					echo '<td> <input type="hidden" name="ItemID" value = '.$row["ItemID"].'> </td>' ;
+					echo '<td> <input type="hidden" name="listID1" value = "'.$listID.'""> </td>' ;
 					echo "<td> <input type='text' name='content' value = '".$row['Content']."''> </td>" ;
 					echo "<td> <input type='text' name='dueDate' value = '".$row['DueDate']."''> </td>" ;
 					echo '<td> <button type="submit" name="update" value="update">update</button> </td>' ;
@@ -83,11 +89,8 @@
 					echo '<td> <button type="submit" name="add" value="add">add</button> </td>' ;
 							echo '</tr>' ;
 							echo '</form>';
-
 						echo '</table>' ;
-
 								
-
 				?>
 			
 		</div>
